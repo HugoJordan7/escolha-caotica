ca823700d8a94b059153a9cebdd3b6d69b88b43a
Hugo Jordan <hugoj8366@gmail.com>
2023-10-25 17:39:50 -0300

    changes to the name card

--- app/src/main/java/com/example/escolhacaotica/MainActivity.kt
+++ app/src/main/java/com/example/escolhacaotica/MainActivity.kt
@@ -12,26 +12,58 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         var editMinimum: EditText = findViewById(R.id.input_cardNumbers_minimum)
         var editMaximum: EditText = findViewById(R.id.input_cardNumbers_maximum)
         var buttonNumbers: Button = findViewById(R.id.button_cardNumbers)
         var txtResultNumbers: TextView = findViewById(R.id.txt_cardNumbers_result)
-
         buttonNumbers.setOnClickListener {
-            var random = Random()
-            var min = editMinimum.text.toString().toInt()
-            var max = editMaximum.text.toString().toInt()
-            if(min>max){
-                Toast.makeText(this,"O valor mínimo deve ser menor que o máximo!",Toast.LENGTH_LONG).show()
-            } else{
-                var numberResult = random.nextInt(max-min+1) + min
-                txtResultNumbers.text = numberResult.toString()
+            if(editMinimum.text.toString().isEmpty() || editMaximum.text.toString().isEmpty()){
+                Toast.makeText(this,R.string.toast_cardNumbers_isEmpty,Toast.LENGTH_SHORT).show()
+            }else {
+                var random = Random()
+                var min = editMinimum.text.toString().toInt()
+                var max = editMaximum.text.toString().toInt()
+                if (min > max) {
+                    Toast.makeText(this, R.string.toast_cardNumbers_invalidMinimum, Toast.LENGTH_SHORT).show()
+                } else {
+                    var numberResult = random.nextInt(max - min + 1) + min
+                    txtResultNumbers.text = numberResult.toString()
+                }
             }
-
         }
-    }
 
-    fun clickButtonNumbers(button: Button){
+        var editName: EditText = findViewById(R.id.input_cardNames_name)
+        var buttonAdd: Button = findViewById(R.id.button_cardNames_add)
+        var buttonSort: Button = findViewById(R.id.button_cardNames_sort)
+        var buttonClear: Button = findViewById(R.id.button_cardNames_clear)
+        var txtResultNames: TextView = findViewById(R.id.txt_cardNames_result)
+        var listNames = mutableSetOf<String>()
 
+        buttonAdd.setOnClickListener {
+            var name = editName.text.toString()
+            if(name.isEmpty()){
+                Toast.makeText(this,R.string.toast_cardNames_isEmpty,Toast.LENGTH_SHORT).show()
+            } else{
+                listNames.add(name)
+            }
+        }
+
+        buttonSort.setOnClickListener {
+            if(listNames.size == 0) {
+                Toast.makeText(this,R.string.toast_cardNames_isEmpty,Toast.LENGTH_SHORT).show()
+            } else{
+                txtResultNames.text = listNames.random()
+            }
+        }
+
+        buttonClear.setOnClickListener {
+            if(listNames.size == 0){
+                Toast.makeText(this,R.string.toast_cardNames_clear,Toast.LENGTH_SHORT).show()
+            } else{
+                listNames.clear()
+                txtResultNames.text = null
+            }
+        }
     }
 }
