9cd7ca11acf4c11ec913c8b7324c16d31428b4a3
Hugo Jordan <hugoj8366@gmail.com>
2023-10-27 10:55:32 -0300

    adding borders in cards and author's mark

--- app/src/main/java/com/example/escolhacaotica/MainActivity.kt
+++ app/src/main/java/com/example/escolhacaotica/MainActivity.kt
@@ -46,6 +46,7 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this,R.string.toast_cardNames_isEmpty,Toast.LENGTH_SHORT).show()
             } else{
                 listNames.add(name)
+                editName.text = null
             }
         }
 
