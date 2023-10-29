f04612bddcdd093adbc99b6d1e5ce05a3cf4960d
Hugo Jordan <hugoj8366@gmail.com>
2023-10-28 22:23:13 -0300

    Adding show of the last number/name sorted

--- app/src/main/java/com/example/escolhacaotica/MainActivity.kt
+++ app/src/main/java/com/example/escolhacaotica/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.escolhacaotica
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -17,6 +18,12 @@ class MainActivity : AppCompatActivity() {
         var editMaximum: EditText = findViewById(R.id.input_cardNumbers_maximum)
         var buttonNumbers: Button = findViewById(R.id.button_cardNumbers)
         var txtResultNumbers: TextView = findViewById(R.id.txt_cardNumbers_result)
+        var prefsNumbers = getSharedPreferences("dataBaseNumbers", Context.MODE_PRIVATE)
+        var prefsNumbersResult = prefsNumbers.getString("lastNumberSorted",null)
+        if(prefsNumbersResult != null){
+            txtResultNumbers.text = getString(R.string.txt_cards_result) + " " +  prefsNumbersResult
+        }
+
         buttonNumbers.setOnClickListener {
             if(editMinimum.text.toString().isEmpty() || editMaximum.text.toString().isEmpty()){
                 Toast.makeText(this,R.string.toast_cardNumbers_isEmpty,Toast.LENGTH_SHORT).show()
@@ -30,6 +37,10 @@ class MainActivity : AppCompatActivity() {
                     var numberResult = random.nextInt(max - min + 1) + min
                     txtResultNumbers.text = numberResult.toString()
                 }
+
+                var editorNumbers = prefsNumbers.edit()
+                editorNumbers.putString("lastNumberSorted",txtResultNumbers.text.toString())
+                editorNumbers.apply()
             }
         }
 
@@ -39,6 +50,11 @@ class MainActivity : AppCompatActivity() {
         var buttonClear: Button = findViewById(R.id.button_cardNames_clear)
         var txtResultNames: TextView = findViewById(R.id.txt_cardNames_result)
         var listNames = mutableSetOf<String>()
+        var prefsNames = getSharedPreferences("dataBaseNames", Context.MODE_PRIVATE)
+        var prefsNamesResult = prefsNames.getString("lastNameSorted",null)
+        if(prefsNamesResult != null){
+            txtResultNames.text = getString(R.string.txt_cards_result) + " " +  prefsNamesResult
+        }
 
         buttonAdd.setOnClickListener {
             var name = editName.text.toString()
@@ -55,6 +71,9 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this,R.string.toast_cardNames_isEmpty,Toast.LENGTH_SHORT).show()
             } else{
                 txtResultNames.text = listNames.random()
+                var editorNames = prefsNames.edit()
+                editorNames.putString("lastNameSorted",txtResultNames.text.toString())
+                editorNames.apply()
             }
         }
 
