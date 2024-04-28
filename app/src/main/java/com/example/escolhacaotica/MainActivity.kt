2bb91361e9cefbfb025bfe8e67d2aa7a655e01b4
Hugo Jordan <hugoj8366@gmail.com>
2024-04-27 22:32:32 -0300

    Implementando viewBinding

--- app/src/main/java/com/example/escolhacaotica/MainActivity.kt
+++ app/src/main/java/com/example/escolhacaotica/MainActivity.kt
@@ -1,88 +1,105 @@
 package com.example.escolhacaotica
 
 import android.content.Context
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import android.view.View
 import android.widget.Toast
+import com.example.escolhacaotica.databinding.ActivityMainBinding
 import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var prefsNumbers: SharedPreferences
+    private lateinit var prefsNames: SharedPreferences
+    var listNames = mutableSetOf<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        var editMinimum: EditText = findViewById(R.id.input_cardNumbers_minimum)
-        var editMaximum: EditText = findViewById(R.id.input_cardNumbers_maximum)
-        var buttonNumbers: Button = findViewById(R.id.button_cardNumbers)
-        var txtResultNumbers: TextView = findViewById(R.id.txt_cardNumbers_result)
         var prefsNumbers = getSharedPreferences("dataBaseNumbers", Context.MODE_PRIVATE)
         var prefsNumbersResult = prefsNumbers.getString("lastNumberSorted",null)
         if(prefsNumbersResult != null){
-            txtResultNumbers.text = getString(R.string.txt_cards_result) + " " +  prefsNumbersResult
+            binding.cardNumbers.txtResult.text = getString(R.string.txt_cards_result) + " " +  prefsNumbersResult
         }
 
-        buttonNumbers.setOnClickListener {
-            if(editMinimum.text.toString().isEmpty() || editMaximum.text.toString().isEmpty()){
-                Toast.makeText(this,R.string.toast_cardNumbers_isEmpty,Toast.LENGTH_SHORT).show()
-            }else {
-                var random = Random()
-                var min = editMinimum.text.toString().toInt()
-                var max = editMaximum.text.toString().toInt()
-                if (min > max) {
-                    Toast.makeText(this, R.string.toast_cardNumbers_invalidMinimum, Toast.LENGTH_SHORT).show()
-                } else {
-                    var numberResult = random.nextInt(max - min + 1) + min
-                    txtResultNumbers.text = numberResult.toString()
-                }
-
-                var editorNumbers = prefsNumbers.edit()
-                editorNumbers.putString("lastNumberSorted",txtResultNumbers.text.toString())
-                editorNumbers.apply()
-            }
-        }
-
-        var editName: EditText = findViewById(R.id.input_cardNames_name)
-        var buttonAdd: Button = findViewById(R.id.button_cardNames_add)
-        var buttonSort: Button = findViewById(R.id.button_cardNames_sort)
-        var buttonClear: Button = findViewById(R.id.button_cardNames_clear)
-        var txtResultNames: TextView = findViewById(R.id.txt_cardNames_result)
-        var listNames = mutableSetOf<String>()
-        var prefsNames = getSharedPreferences("dataBaseNames", Context.MODE_PRIVATE)
+        prefsNames = getSharedPreferences("dataBaseNames", Context.MODE_PRIVATE)
         var prefsNamesResult = prefsNames.getString("lastNameSorted",null)
         if(prefsNamesResult != null){
-            txtResultNames.text = getString(R.string.txt_cards_result) + " " +  prefsNamesResult
+            binding.cardNames.txtResult.text = getString(R.string.txt_cards_result) + " " + prefsNamesResult
         }
 
-        buttonAdd.setOnClickListener {
-            var name = editName.text.toString()
-            if(name.isEmpty()){
-                Toast.makeText(this,R.string.toast_cardNames_isEmpty,Toast.LENGTH_SHORT).show()
+        binding.cardNumbers.buttonSorted.setOnClickListener(this)
+        binding.cardNames.buttonAdd.setOnClickListener(this)
+        binding.cardNames.buttonClear.setOnClickListener(this)
+        binding.cardNames.buttonSort.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            binding.cardNumbers.buttonSorted.id -> numberButtonResultListener()
+            binding.cardNames.buttonAdd.id -> namesButtonAddListener()
+            binding.cardNames.buttonClear.id -> namesButtonClearListener()
+            binding.cardNames.buttonSort.id -> namesButtonSortListener()
+        }
+    }
+
+
+    private fun numberButtonResultListener(){
+        binding.cardNumbers.apply {
+            if(binding.cardNumbers.editMinimum.text.toString().isEmpty() ||
+                binding.cardNumbers.editMaximum.text.toString().isEmpty()){
+                Toast.makeText(this@MainActivity,R.string.toast_cardNumbers_isEmpty,Toast.LENGTH_SHORT).show()
+                return
+            }
+            var random = Random()
+            var min = editMinimum.text.toString().toInt()
+            var max = editMaximum.text.toString().toInt()
+            if (min > max) {
+                Toast.makeText(this@MainActivity, R.string.toast_cardNumbers_invalidMinimum, Toast.LENGTH_SHORT).show()
+            } else {
+                var numberResult = random.nextInt(max - min + 1) + min
+                txtResult.text = numberResult.toString()
+            }
+            var editorNumbers = prefsNumbers.edit()
+            editorNumbers.putString("lastNumberSorted",txtResult.text.toString())
+            editorNumbers.apply()
+        }
+    }
+
+    private fun namesButtonAddListener(){
+        binding.cardNames.editName.text.apply {
+            if(toString().isEmpty()){
+                Toast.makeText(this@MainActivity,R.string.toast_cardNames_isEmpty,Toast.LENGTH_SHORT).show()
             } else{
-                listNames.add(name)
-                editName.text = null
+                listNames.add(toString())
+                null
             }
         }
+    }
 
-        buttonSort.setOnClickListener {
-            if(listNames.size == 0) {
-                Toast.makeText(this,R.string.toast_cardNames_isEmpty,Toast.LENGTH_SHORT).show()
-            } else{
-                txtResultNames.text = listNames.random()
+    private fun namesButtonClearListener(){
+        if(listNames.size == 0){
+            Toast.makeText(this,R.string.toast_cardNames_clear,Toast.LENGTH_SHORT).show()
+        } else{
+            listNames.clear()
+            binding.cardNames.txtResult.text = null
+        }
+    }
+
+    private fun namesButtonSortListener(){
+        binding.cardNames.txtResult.text.apply {
+            if (listNames.size == 0) {
+                Toast.makeText(this@MainActivity, R.string.toast_cardNames_isEmpty, Toast.LENGTH_SHORT).show()
+            } else {
+                listNames.random()
                 var editorNames = prefsNames.edit()
-                editorNames.putString("lastNameSorted",txtResultNames.text.toString())
+                editorNames.putString("lastNameSorted", toString())
                 editorNames.apply()
-            }
-        }
-
-        buttonClear.setOnClickListener {
-            if(listNames.size == 0){
-                Toast.makeText(this,R.string.toast_cardNames_clear,Toast.LENGTH_SHORT).show()
-            } else{
-                listNames.clear()
-                txtResultNames.text = null
             }
         }
     }
